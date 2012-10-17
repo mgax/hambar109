c6b4d905893adb7e3a7a02ec1a6f1b8333d30d56
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-10-17 18:59:14 +0300

    [fix] use celery with redis

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,8 @@
 import os
 
-BROKER_URL = 'sqla+sqlite:///celerydb.sqlite'
-CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = BROKER_URL
+CELERY_IMPORTS = ('search', 'manage')
+BROKER_URL = 'redis://localhost:{PUBDOCS_REDIS_PORT}/0'.format(**os.environ)
+CELERY_RESULT_BACKEND = BROKER_URL
 
 #sometimes PUBDOCS_DB_USED is None sometimes is sqlite
 if os.environ.get('PUBDOCS_DB_USED')=='sqlite':
