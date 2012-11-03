0f68057e9278d97f77654be9ba2395e7b29645c4
Alex Morega <alex@grep.ro>
2012-11-03 20:22:58 +0200

    rename variable

--- mof_parser/mof_parser.py
+++ mof_parser/mof_parser.py
@@ -37,36 +37,36 @@ class MofParser(object):
             yield p.text_content().strip().encode('utf-8')
 
     def parse(self):
-        section = {}
+        meta = {}
         state = 'expect_heading'
         for line in self._iter_lines():
             line = self._tags.sub('', line).strip()
 
             if state == 'expect_heading':
                 if 'P A R T E A' in line:
-                    section['heading'] = 'PARTEA I'
+                    meta['heading'] = 'PARTEA I'
                     state = 'expect_title'
                 continue
 
             if state == 'expect_title':
                 if line:
-                    section['title'] = line
+                    meta['title'] = line
                     state = 'expect_identifier'
                 continue
 
             if state == 'expect_identifier':
                 if line:
-                    section['identifier'] = line
+                    meta['identifier'] = line
                     state = 'expect_content'
                     break
 
         # "Anul 177 (XXI) — Nr. 174 Joi, 19 martie 2009"
-        pre_text, date_text = section['identifier'].rsplit(',', 1)
+        pre_text, date_text = meta['identifier'].rsplit(',', 1)
         day, month_name, year = date_text.split()
-        section['date'] = date(int(year), MONTH[month_name], int(day))
-        section['mof_number'] = int(pre_text.split()[-2])
+        meta['date'] = date(int(year), MONTH[month_name], int(day))
+        meta['mof_number'] = int(pre_text.split()[-2])
 
-        return [section]
+        return [meta]
 
 
 def parse(text):
