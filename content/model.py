9dffab9255d67cdc921630c418c89a00f5f65012
Alex Morega <alex@grep.ro>
2012-11-12 22:56:49 +0200

    update readme; change initial schema

--- content/model.py
+++ content/model.py
@@ -1,8 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 import sqlalchemy as sa
 
 
 Base = declarative_base()
+
+
+class Document(Base):
+
+    __tablename__ = 'documents'
+    id = sa.Column(sa.Integer, primary_key=True)
+    code = sa.Column(sa.String)
+    import_time = sa.Column(sa.DateTime)
 
 
 class ActType(Base):
@@ -13,19 +22,24 @@ class ActType(Base):
     label = sa.Column(sa.String)
 
 
-class Source(Base):
-
-    __tablename__ = 'sources'
-    id = sa.Column(sa.Integer, primary_key=True)
-    code = sa.Column(sa.String)
-
-
 class Act(Base):
 
     __tablename__ = 'acts'
     id = sa.Column(sa.Integer, primary_key=True)
     type_id = sa.Column(sa.Integer, sa.ForeignKey('act_types.id'))
-    source_id = sa.Column(sa.Integer, sa.ForeignKey('sources.id'))
+    type = relationship("ActType")
+    document_id = sa.Column(sa.Integer, sa.ForeignKey('documents.id'))
+    document = relationship("Document")
     ident = sa.Column(sa.String)
     title = sa.Column(sa.String)
     text = sa.Column(sa.Text)
+
+
+class ImportResult(Base):
+
+    __tablename__ = 'import_result'
+    id = sa.Column(sa.Integer, primary_key=True)
+    time = sa.Column(sa.DateTime)
+    document_id = sa.Column(sa.Integer, sa.ForeignKey('documents.id'))
+    document = relationship("Document")
+    success = sa.Column(sa.Boolean)
