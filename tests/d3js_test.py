c21b3a010fc22ef03040bebb26775a8f52a78360
Alex Morega <alex@grep.ro>
2012-11-19 09:58:54 +0100

    remove unneeded test

--- tests/d3js_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from path import path
-from search import available_files, construct_tree
-
-class RepoTest(unittest.TestCase):
-
-    def test_available_files(self):
-        tree = construct_tree(path('/home/mihai/Work/pubdocs/tests/repo'))
-        import pdb; pdb.set_trace()
