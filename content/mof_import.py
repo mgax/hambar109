cb5e37fc694552ef25e243543d8c32840c240e73
Alex Morega <alex@grep.ro>
2012-11-21 21:55:48 +0200

    quickfix: explicitly configure celery

--- content/mof_import.py
+++ content/mof_import.py
@@ -150,6 +150,8 @@ def register_commands(manager):
     @manager.option('-w', '--as-worker', action='store_true',
                     help="Run as worker task")
     def mof_import(name, raw_html=False, as_json=False, as_worker=False):
+        import harvest
+        harvest.configure_celery()
         if name == '-':
             for line in sys.stdin:
                 mof_import(line.strip(), raw_html, as_json, as_worker)
