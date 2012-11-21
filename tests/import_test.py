be89feba2425a236ca44fe4d6637459c65913e6b
Alex Morega <alex@grep.ro>
2012-11-21 21:28:35 +0200

    make sure other act fields are saved

--- tests/import_test.py
+++ tests/import_test.py
@@ -19,3 +19,23 @@ class ImportTest(unittest.TestCase):
         with db_session(self.app) as session:
             [doc] = session.query(Document).all()
             self.assertEqual(doc.code, 'mof1_2007_0123')
+
+    def test_act_is_created_on_save(self):
+        from content.mof_import import save_document_acts
+        from content.model import Document
+
+        acts = [{
+            'section': 'hg',
+            'number': '13',
+            'title': "HG 13 !!!",
+        }]
+
+        with self.app.app_context():
+            save_document_acts(acts, 'mof1_2007_0123')
+
+        with db_session(self.app) as session:
+            [doc] = session.query(Document).all()
+            [act13] = doc.acts
+            self.assertEqual(act13.title, "HG 13 !!!")
+            self.assertEqual(act13.ident, '13')
+            self.assertEqual(act13.type.code, 'hg')
