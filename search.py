be4837d052447b457d97d0bcb90453e1e8968279
Alex Morega <alex@grep.ro>
2013-02-12 23:51:53 +0200

    move ElasticSearch class to hambar.elastic module

--- search.py
+++ search.py
@@ -18,41 +18,12 @@ from pyquery import PyQuery as pq
 import utils
 from html2text import html2text
 from hambar.tika import invoke_tika
+from hambar.elastic import ElasticSearch
 
 DEBUG_SEARCH = (os.environ.get('DEBUG_SEARCH') == 'on')
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG if DEBUG_SEARCH else logging.INFO)
-
-
-class ElasticSearch(object):
-
-    def __init__(self, api_url):
-        self.api_url = api_url
-
-    def search(self, text, fields=None, page=1, per_page=20):
-        search_data = {
-            "fields": ["title"],
-            "query": {
-                "query_string": {"query": text},
-            },
-            "highlight": {
-                "fields": {"file": {}},
-            },
-        }
-        search_url = self.api_url + '/_search'
-        search_url += '?from=%d&size=%d' % ((page - 1) * per_page, per_page)
-        if fields is not None:
-            search_url += '&fields=' + ','.join(fields)
-        search_json = flask.json.dumps(search_data,
-                                       indent=2 if DEBUG_SEARCH else None)
-        log.debug("search: %s", search_json)
-        search_resp = requests.get(search_url, data=search_json)
-        if search_resp.status_code != 200:
-            log.error("Error response: %r", search_resp.text)
-            raise RuntimeError("ElasticSearch query failed")
-        assert search_resp.status_code == 200, repr(search_resp)
-        return search_resp.json
 
 
 es = ElasticSearch(os.environ.get('PUBDOCS_ES_URL'))
