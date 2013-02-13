dc2831fdabc11f0c2f7e781bab9bc9b7086df2ad
Alex Morega <alex@grep.ro>
2013-02-13 10:36:46 +0200

    log to sentry

--- manage.py
+++ manage.py
@@ -93,4 +93,11 @@ def tornado(port):
 
 if __name__ == '__main__':
     logging.basicConfig(format=LOG_FORMAT)
+
+    SENTRY_DSN = os.environ.get('SENTRY_DSN')
+    if SENTRY_DSN:
+        from raven.conf import setup_logging
+        from raven.handlers.logging import SentryHandler
+        setup_logging(SentryHandler(SENTRY_DSN, level=logging.WARN))
+
     manager.run()
