018c7624611ba8905e4a51d4f67902bf96209b66
Alex Morega <alex@grep.ro>
2013-09-16 02:17:44 +0300

    set fetchme flag as needed

--- hambar/harvest.py
+++ hambar/harvest.py
@@ -6,7 +6,8 @@ harvest_manager = Manager()
 
 @harvest_manager.option('number', type=int)
 @harvest_manager.option('part', type=int)
-def new_editions(part, number):
+@harvest_manager.option('-f', '--fetch', action='store_true')
+def new_editions(part, number, fetch=False):
     year = 2013
     latest_known = (model.Mof.query
                              .filter_by(year=year, part=part)
@@ -15,5 +16,8 @@ def new_editions(part, number):
     next_number = 1 if latest_known is None else latest_known.number + 1
     print next_number, number
     for number in range(next_number, number + 1):
-        model.db.session.add(model.Mof(year=year, part=part, number=number))
+        row = model.Mof(year=year, part=part, number=number)
+        if fetch:
+            row.fetchme = True
+        model.db.session.add(row)
     model.db.session.commit()
