b5abfe5df042c33045739d49ff9a0b72fdb9a070
Alex Morega <alex@grep.ro>
2013-09-16 02:58:02 +0300

    download many files with sleep

--- hambar/harvest.py
+++ hambar/harvest.py
@@ -1,7 +1,22 @@
+import sys
+import random
+import logging
+import time
+import flask
 from flask.ext.script import Manager
+from werkzeug.wsgi import FileWrapper
+import requests
+from path import path
 from hambar import model
 
 harvest_manager = Manager()
+
+URL_FORMAT = ('http://www.monitoruloficial.ro/emonitornew/php/services'
+              '/view.php?doc=05{mof.year}{mof.number}&format=pdf')
+FILENAME_FORMAT = 'mof{mof.part}_{mof.year}_{mof.number:04}.pdf'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 @harvest_manager.option('number', type=int)
@@ -21,3 +36,35 @@ def new_editions(part, number, fetch=False):
             row.fetchme = True
         model.db.session.add(row)
     model.db.session.commit()
+
+
+@harvest_manager.option('count', type=int)
+def fetch(count):
+    harvest_path = path(flask.current_app.instance_path) / 'harvest'
+    harvest_path.makedirs_p()
+
+    for c in xrange(count):
+        model.db.session.rollback()
+        mof_pool = model.Mof.query.filter_by(fetchme=True, part=4)
+        lucky = random.randrange(mof_pool.count())
+        mof = mof_pool.offset(lucky).first()
+        url = URL_FORMAT.format(mof=mof)
+        file_path = harvest_path / FILENAME_FORMAT.format(mof=mof)
+        mof.fetchme = False
+        model.db.session.commit()
+
+        if file_path.exists():
+            logger.info("Skipping %s, already exists", file_path.name)
+            continue
+
+        logger.info("Fetching %s to %s", url, file_path.name)
+
+        with file_path.open('wb') as f:
+            agent_url = flask.current_app.config['HAMBAR_AGENT_URL']
+            resp = requests.post(agent_url, data={'url': url}, stream=True)
+            for chunk in FileWrapper(resp.raw):
+                f.write(chunk)
+
+        t = random.randint(20, 30)
+        logger.info("Sleeping for %d secods", t)
+        time.sleep(t)
